e77fc19833fb6d6577902e71c34daa7007a48649
Prakash <prakash@quintype.com>
2016-05-16 19:37:22 +0530

    removes unused files and folders

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -5,20 +5,5 @@ import static ratpack.groovy.Groovy.ratpack
 import db.DbConfig
 
 ratpack {
-  bindings {
-    module MarkupTemplateModule
-  }
 
-  handlers {
-    get {
-      DbConfig.wrapInTransaction { txn ->
-	println "inside closure"
-	println txn;
-	println "done";
-      }
-      render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
-    }
-
-    files { dir "public" }
-  }
 }
